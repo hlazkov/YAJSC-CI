04925bcd0520e50a8fe27050c1b46665fc819601
dmorgun <daryamorgun@gmail.com>
2023-04-26 07:01:48 -0500

    Functions theory finished

--- Unit-3/functions_theory.js
+++ Unit-3/functions_theory.js
@@ -67,3 +67,50 @@ function myFunc2 (a,b,c) {
 }
 
 console.log(myFunc2(1,2));
+
+// scopes of visibility
+const aa = 100500;
+
+function myFunc3 (aa, b, c=10) {
+    console.log("Inside aa = " + aa);
+    console.log("Inside b = " + b);
+    console.log("Inside c = " + c);
+}
+
+console.log("Outside aa = " + aa);
+
+myFunc3(1,2);
+
+
+// function declaration
+function myFunc4(a, b, c = 10) {
+    return a + b + c;
+}
+
+// function expression
+const x = function(a, b, c = 10) {
+    return a + b + c;
+}
+
+// arrow function
+let y = (a, b, c = 10) => {
+    return a + b + c;
+}
+
+// {} and "return" can be ommited in arrow functions:
+y = (a, b, c = 10) => a + b + c;
+
+
+// callback
+
+function myFunc5 (a, b, cb) {
+    ///
+    ///
+    return cb (a, b) + 10;
+}
+
+const myFunc6 = (p1, p2) => {
+    return p1 + p2
+}
+
+console.log(myFunc5(10, 5, myFunc6));
