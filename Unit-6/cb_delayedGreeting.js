cb3e52bf479b4c9ef04fa55706d603c5e90327c5
dmorgun <daryamorgun@gmail.com>
2023-05-29 02:00:51 -0500

    some changes

--- Unit-6/cb_delayedGreeting.js
+++ Unit-6/cb_delayedGreeting.js
@@ -6,15 +6,15 @@ const delayedGreeting = (name, callback) => {
     let resultOfTheAsyncFunc = `Hello, ${name}!`;
     console.log('test0');
     setTimeout(() => {
-        console.log('test1');
-        callback(resultOfTheAsyncFunc);
         console.log('test2');
+        callback(resultOfTheAsyncFunc);
         console.log('test3');
         console.log('test4');
+        console.log('test5');
 
     }, 10000);
 };
 
 delayedGreeting('Alice', someFuncWithParam);
-console.log('test5');
+console.log('test1');
 // node ./Unit-6/cb_delayedGreeting.js
