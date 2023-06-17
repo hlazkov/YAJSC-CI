bf28adc4def5023aabf6d0e0849c140a0e742177
dmorgun <daryamorgun@gmail.com>
2023-06-17 02:13:35 -0500

    Added organizing tests with mocha

---
+++ Unit-9/myScript.spec.js
@@ -0,0 +1,25 @@
+//organizing tests with mocha:
+
+describe('Test Name Suite1', () => {
+    
+    it('Test1 Name', () = {
+        return myPromise();
+            .then((res) => () {
+
+        });
+    });
+
+    it('Test2 Name', () = {
+        return myPromise();
+            .then((res) => () {
+
+        });
+    });
+
+    it('Test3 Name', () = {
+        return myPromise();
+            .then((res) => () {
+
+        });
+    });
+})
